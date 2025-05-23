ca4bebfabd837e8dc6911ed09f69fff814f79a95
Izhanjunaid <izhanjunaid123@gmail.com>
2025-05-24 00:42:06 +0500

    dynamic products

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
 // API Configuration
 export const API_CONFIG = {
   BASE_URL: 'http://139.59.85.26:5000',
-  API_URL: 'https://transfermakeup.duckdns.org',
-  MAKEUP_API_URL: 'http://localhost:8000',
+  API_URL: 'https://makeupmongo.duckdns.org/api',
+  MAKEUP_API_URL: 'https://transfermakeup.duckdns.org',
   IMAGE_DOMAINS: ['localhost', '139.59.85.26','makeupmongo.duckdns.org'],
   MONGODB_URI: 'mongodb://doadmin:izhan177k@db-mongodb-nyc1-12345-a0c5c5c5.mongodb.net:27017/ecommerceDB?authSource=admin&replicaSet=db-mongodb-nyc1-12345&tls=true&tlsCAFile=/etc/ssl/certs/ca-certificates.crt'
 };
