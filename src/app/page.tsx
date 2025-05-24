3511812526e38940e9630e0147d61abbaea44b55
Izhanjunaid <izhanjunaid123@gmail.com>
2025-05-24 23:01:31 +0500

    new-gpu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,11 @@ import Category from "@/components/Category";
 import Feature from "@/components/Feature";
 import Hero from "@/components/Hero";
 import NewArrival from "@/components/NewArrival";
-import GlobalMakeupFeature from "@/components/GlobalMakeupFeature";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <div className="container mx-auto px-4 py-8">
-        <GlobalMakeupFeature />
-      </div>
       <Feature />
       <NewArrival />
       <Category />
