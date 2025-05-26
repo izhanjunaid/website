dd5b420a5e8675a1ece75125b0977e5add662a6a
izhanjunaid <izhanjunaid123@gmail.com>
2025-05-26 10:52:07 +0500

    final website

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@
 export const API_CONFIG = {
   BASE_URL: 'http://139.59.85.26:5000',
   API_URL: 'https://makeupmongo.duckdns.org/api',
-  MAKEUP_API_URL: 'https://4279-119-73-121-7.ngrok-free.app',
+  MAKEUP_API_URL: 'https://5da5-2407-aa80-116-2757-949-2e4a-1a79-771b.ngrok-free.app',
   IMAGE_DOMAINS: ['localhost', '139.59.85.26','makeupmongo.duckdns.org'],
   MONGODB_URI: 'mongodb://doadmin:izhan177k@db-mongodb-nyc1-12345-a0c5c5c5.mongodb.net:27017/ecommerceDB?authSource=admin&replicaSet=db-mongodb-nyc1-12345&tls=true&tlsCAFile=/etc/ssl/certs/ca-certificates.crt'
 };
