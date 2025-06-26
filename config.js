03e392c91136a75b80c1603a347440592790c013
izhanjunaid <izhanjunaid123@gmail.com>
2025-06-26 12:06:45 +0500

    interview

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const API_CONFIG = {
   BASE_URL: 'http://139.59.85.26:5000',
   API_URL: 'https://makeupmongo.duckdns.org/api',
-  MAKEUP_API_URL: 'https://ca9b-2407-aa80-116-d989-c9c2-b1ef-f44e-84d3.ngrok-free.app',
+  MAKEUP_API_URL: 'https://595c-2407-aa80-116-eb04-8568-ae1b-c62a-e3fa.ngrok-free.app',
   IMAGE_DOMAINS: ['localhost', '139.59.85.26','makeupmongo.duckdns.org'],
   MONGODB_URI: 'mongodb://doadmin:izhan177k@db-mongodb-nyc1-12345-a0c5c5c5.mongodb.net:27017/ecommerceDB?authSource=admin&replicaSet=db-mongodb-nyc1-12345&tls=true&tlsCAFile=/etc/ssl/certs/ca-certificates.crt'
 };
